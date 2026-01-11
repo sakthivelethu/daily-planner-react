e315797c1055924d366966177f6b3b57d1b36599
sakthivelethu <sakthivelrajethu@gmail.com>
2026-01-11 18:16:50 +0530

    deployment fix1

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "../server/routes";
+import { createServer } from "http";
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+(async () => {
+  await registerRoutes(httpServer, app);
+})();
+
+export default app;
